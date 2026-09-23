6a92646865e4b9076a92fd0f62b07d5461d57cef
BrandonAR <101993481+barlino30@users.noreply.github.com>
2026-09-23 17:16:51 -0500

    actualidad

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send('holitassss');
+    res.render('home/home');
 });
 
 module.exports = router;
